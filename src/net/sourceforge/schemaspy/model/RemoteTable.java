1ac2bd7519c6210b960c04bbfd6ba6f3c7b726f2
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2006-12-07 16:35:52 +0000

    First test of cross-schema work
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@299 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/model/RemoteTable.java
+++ src/net/sourceforge/schemaspy/model/RemoteTable.java
@@ -3,6 +3,9 @@ package net.sourceforge.schemaspy.model;
 import java.sql.*;
 
 /**
+ * A table that's outside of the default schema but is referenced
+ * by or references a table in the default schema.
+ * 
  * @author John Currier
  */
 public class RemoteTable extends Table {
