97211c2763e470f6b9248d32251ae4d0183c53be
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2006-12-13 00:26:34 +0000

    First pass at a rudimentary UI
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@311 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/ui/MainFrame.java
+++ src/net/sourceforge/schemaspy/ui/MainFrame.java
@@ -3,6 +3,12 @@ package net.sourceforge.schemaspy.ui;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 /**
  * @author John Currier
@@ -11,6 +17,8 @@ public class MainFrame extends JFrame {
     private static final long serialVersionUID = 1L;
     private JPanel jContentPane = null;
     private JPanel dbConfigPanel = null;
+    private JPanel buttonBar = null;
+    private JButton launchButton = null;
 
     /**
      * This is the default constructor
@@ -28,7 +36,7 @@ public class MainFrame extends JFrame {
     private void initialize() {
         this.setContentPane(getJContentPane());
         this.setTitle("SchemaSpy");
-        this.setSize(new Dimension(500, 250));
+        this.setSize(new Dimension(500, 281));
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
@@ -53,8 +61,36 @@ public class MainFrame extends JFrame {
             jContentPane = new JPanel();
             jContentPane.setLayout(new BorderLayout());
             jContentPane.add(getDbConfigPanel(), BorderLayout.NORTH);
+            jContentPane.add(getButtonBar(), BorderLayout.SOUTH);
         }
         return jContentPane;
+    }
+
+    /**
+     * This method initializes buttonBar	
+     * 	
+     * @return javax.swing.JPanel	
+     */
+    private JPanel getButtonBar() {
+        if (buttonBar == null) {
+            buttonBar = new JPanel();
+            buttonBar.setLayout(new FlowLayout(FlowLayout.TRAILING));
+            buttonBar.add(getLaunchButton(), null);
+        }
+        return buttonBar;
+    }
+
+    /**
+     * This method initializes launchButton	
+     * 	
+     * @return javax.swing.JButton	
+     */
+    private JButton getLaunchButton() {
+        if (launchButton == null) {
+            launchButton = new JButton();
+            launchButton.setText("Launch");
+        }
+        return launchButton;
     }
 
 }  //  @jve:decl-index=0:visual-constraint="10,10"
