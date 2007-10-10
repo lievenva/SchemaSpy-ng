90ccc4b9e8e61a4c69b3e36ba1258b23d1d32ddd
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2007-10-10 22:52:28 +0000

    Prevent the gui from being displayed accidentally.
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@362 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/Main.java
+++ src/net/sourceforge/schemaspy/Main.java
@@ -7,7 +7,7 @@ import net.sourceforge.schemaspy.ui.*;
  */
 public class Main {
     public static void main(String[] argv) throws Exception {
-        if (argv.length == 0) {
+        if (argv.length == 1 && argv[0].equals("-gui")) { // warning: serious temp hack
             new MainFrame().setVisible(true);
             return;
         }
