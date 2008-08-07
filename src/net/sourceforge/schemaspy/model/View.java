122bd9bc10112f6e41daf0a3f67f1c937e8995b0
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2008-08-07 21:44:47 +0000

    Wasn't a great idea to do view sql query once vs. once per view.
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@388 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/model/View.java
+++ src/net/sourceforge/schemaspy/model/View.java
@@ -1,19 +1,22 @@
 package net.sourceforge.schemaspy.model;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class View extends Table {
-    private String viewSql;
+    private final String viewSql;
 
     /**
      * @param db
      * @param rs
+     * @param selectViewSql
      * @throws java.sql.SQLException
      */
-    public View(Database db, ResultSet rs) throws SQLException {
+    public View(Database db, ResultSet rs, String selectViewSql) throws SQLException {
         super(db, rs.getString("TABLE_SCHEM"), rs.getString("TABLE_NAME"), db.getOptionalString(rs, "REMARKS"), null);
+        viewSql = getViewSql(db, selectViewSql);
     }
 
     /**
@@ -24,10 +27,6 @@ public class View extends Table {
         return true;
     }
 
-    public void setViewSql(String viewSql) {
-        this.viewSql = viewSql;
-    }
-
     @Override
     public String getViewSql() {
         return viewSql;
@@ -36,5 +35,29 @@ public class View extends Table {
     @Override
     protected int fetchNumRows(Database database, Properties properties) {
         return 0;
+    }
+
+    private String getViewSql(Database db, String selectViewSql) throws SQLException {
+        if (selectViewSql == null)
+            return null;
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = db.prepareStatement(selectViewSql, getName());
+            rs = stmt.executeQuery();
+            while (rs.next())
+                return rs.getString("text");
+            return null;
+        } catch (SQLException sqlException) {
+            System.err.println(selectViewSql);
+            throw sqlException;
+        } finally {
+            if (rs != null)
+                rs.close();
+            if (stmt != null)
+                stmt.close();
+        }
     }
 }
