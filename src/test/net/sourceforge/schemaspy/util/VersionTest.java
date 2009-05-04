2d41e97958bc12c8a7edad76259144a1109419b1
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2009-05-04 15:49:44 +0000

    Additional version extraction test cases to try to figure out Mac version issues
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@537 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/test/net/sourceforge/schemaspy/util/VersionTest.java
+++ src/test/net/sourceforge/schemaspy/util/VersionTest.java
@@ -1,5 +1,7 @@
 package test.net.sourceforge.schemaspy.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import junit.framework.TestCase;
 import net.sourceforge.schemaspy.util.Version;
 
@@ -33,4 +35,26 @@ public class VersionTest extends TestCase {
         assertFalse(twoTenOne.equals(twoTen));
     }
 
+    public void testExtract2203() {
+        compareEquals("2.20.3", "dot - Graphviz version 2.20.3 (Wed Oct  8 06:02:12 UTC 2008");
+    }
+
+    public void testExtractMac2222() {
+        compareEquals("2.22.2", "dot - graphviz version 2.22.2 (20090313.1817)");
+    }
+    
+    private void compareEquals(String digits, String versionLine)
+    {
+        Version expected = new Version(digits);
+        String actual = getVersionDigits(versionLine);
+        assertEquals(expected, new Version(actual));
+    }
+
+    private String getVersionDigits(String versionLine) {
+        Matcher matcher = Pattern.compile("[0-9][0-9.]+").matcher(versionLine);
+        if (matcher.find())
+            return matcher.group();
+        fail("Failed to extract version digits from " + versionLine);
+        return null;
+    }
 }
