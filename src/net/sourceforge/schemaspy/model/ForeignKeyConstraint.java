2e68c199c03ee12c8de2875159b9219455106e5d
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2009-09-14 16:19:37 +0000

    Updated docs
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@549 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/model/ForeignKeyConstraint.java
+++ src/net/sourceforge/schemaspy/model/ForeignKeyConstraint.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents a <a href='http://en.wikipedia.org/wiki/Foreign_key'>
+ * Foreign Key Constraint</a> that "ties" a child table to a parent table
+ * via foreign and primary keys.
+ */
 public class ForeignKeyConstraint {
     private final String name;
     private Table parentTable;
@@ -13,18 +18,25 @@ public class ForeignKeyConstraint {
     private final char deleteRule;
     private final char updateRule;
 
+    /**
+     * Construct a foreign key for the specified child table.
+     * Relationship details will be added later.
+     *
+     * @param child
+     * @param name
+     */
     ForeignKeyConstraint(Table child, String name) {
         this.name = name; // implied constraints will have a null name and override getName()
         childTable = child;
         deleteRule = 'D';
         updateRule = 'U';
     }
-    
+
     /**
      * This constructor is intended for use <b>after</b> all of the tables have been
      * found in the system.  One impact of using this constructor is that it will
      * "glue" the two tables together through their columns.
-     * 
+     *
      * @param parentColumn
      * @param childColumn
      */
@@ -38,6 +50,11 @@ public class ForeignKeyConstraint {
         parentColumn.addChild(childColumn, this);
     }
 
+    /**
+     * Add a "parent" side to the constraint.
+     *
+     * @param column
+     */
     void addParentColumn(TableColumn column) {
         if (column != null) {
             parentColumns.add(column);
@@ -45,60 +62,112 @@ public class ForeignKeyConstraint {
         }
     }
 
+    /**
+     * Add a "child" side to the constraint.
+     *
+     * @param column
+     */
     void addChildColumn(TableColumn column) {
         if (column != null)
             childColumns.add(column);
     }
 
+    /**
+     * Returns the name of the constraint
+     *
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the parent table (the table that contains the referenced primary key
+     * column).
+     *
+     * @return
+     */
     public Table getParentTable() {
         return parentTable;
     }
 
+    /**
+     * Returns all of the primary key columns that are referenced by this constraint.
+     *
+     * @return
+     */
     public List<TableColumn> getParentColumns() {
         return Collections.unmodifiableList(parentColumns);
     }
 
+    /**
+     * Returns the table on the "child" end of the relationship (contains the foreign
+     * key that references the parent table's primary key).
+     *
+     * @return
+     */
     public Table getChildTable() {
         return childTable;
     }
 
+    /**
+     * Returns all of the foreign key columns that are referenced by this constraint.
+     *
+     * @return
+     */
     public List<TableColumn> getChildColumns() {
         return Collections.unmodifiableList(childColumns);
     }
 
+    /**
+     * Returns the delete rule for this constraint.
+     *
+     * @return
+     */
     public char getDeleteRule() {
+        //TODO Needs to be further clarified / implemented.
         return deleteRule;
     }
 
     /**
+     * Returns <code>true</code> if this constraint should
+     * <a href='http://en.wikipedia.org/wiki/Cascade_delete'>cascade deletions</code>.
+     *
      * @return
      */
     public boolean isOnDeleteCascade() {
-        return deleteRule == 'C';
+        return getDeleteRule() == 'C';
     }
 
+    /**
+     * Returns the update rule for this constraint.
+     *
+     * @return
+     */
     public char getUpdateRule() {
+        //TODO Needs to be further clarified / implemented.
         return updateRule;
     }
 
     /**
+     * Returns <code>true</code> if this is an implied constraint or
+     * <code>false</code> if it is "real".
+     *
+     * Subclasses that implement implied constraints should override this method.
+     *
      * @return
      */
     public boolean isImplied() {
         return false;
     }
-    
+
     /**
-     * We have several types of constraints.  
+     * We have several types of constraints.
      * This returns <code>true</code> if the constraint came from the database
-     * metadata and not inferred by something else.  
+     * metadata and not inferred by something else.
      * This is different than {@link #isImplied()} in that implied relationships
      * are a specific type of non-real relationships.
-     *  
+     *
      * @return
      */
     public boolean isReal() {
@@ -106,6 +175,9 @@ public class ForeignKeyConstraint {
     }
 
     /**
+     * Static method that returns a string representation of the specified
+     * list of {@link TableColumn columns}.
+     *
      * @param columns
      * @return
      */
@@ -116,6 +188,8 @@ public class ForeignKeyConstraint {
     }
 
     /**
+     * Returns a string representation of this foreign key constraint.
+     *
      * @return
      */
     @Override
