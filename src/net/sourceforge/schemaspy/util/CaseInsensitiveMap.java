9403aa5a222c0c3213c5607b8f0b565d598cff7a
johncurrier <johncurrier@e86a8a9b-1e1f-0410-8769-81452b7583c3>
2010-08-11 19:04:32 +0000

    Corrected generic mapping
    git-svn-id: https://schemaspy.svn.sourceforge.net/svnroot/schemaspy/trunk@585 e86a8a9b-1e1f-0410-8769-81452b7583c3

--- src/net/sourceforge/schemaspy/util/CaseInsensitiveMap.java
+++ src/net/sourceforge/schemaspy/util/CaseInsensitiveMap.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * A {@link HashMap} implementation that uses {@link String}s as its keys
  * where the keys are treated without regard to case.  That is, <code>get("MyTableName")</code>
  * will return the same object as <code>get("MYTABLENAME")</code>.
- * 
+ *
  * @author John Currier
  */
 public class CaseInsensitiveMap<V> extends HashMap<String, V>
@@ -19,28 +19,28 @@ public class CaseInsensitiveMap<V> extends HashMap<String, V>
     public V get(Object key) {
         return super.get(((String)key).toUpperCase());
     }
-    
+
     @Override
     public V put(String key, V value) {
         return super.put(key.toUpperCase(), value);
     }
 
     @Override
-    @SuppressWarnings("unchecked") // until I get the iter declaration correct
     public void putAll(Map<? extends String, ? extends V> map) {
-        Iterator iter = map.entrySet().iterator();
-        
+        Iterator<? extends Map.Entry<? extends String, ? extends V>> iter
+                        = map.entrySet().iterator();
+
         while (iter.hasNext()) {
-            Map.Entry<String, V> entry = (Map.Entry<String, V>)iter.next();
-            put(entry.getKey(), entry.getValue());
+            Map.Entry<? extends String, ? extends V> e = iter.next();
+            put(e.getKey(), e.getValue());
         }
     }
-    
+
     @Override
     public V remove(Object key) {
         return super.remove(((String)key).toUpperCase());
     }
-    
+
     @Override
     public boolean containsKey(Object key) {
         return super.containsKey(((String)key).toUpperCase());
