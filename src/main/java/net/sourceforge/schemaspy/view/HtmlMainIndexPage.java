232bab3312ef573cefdc9f59b00e4e0082cbc228
Lieven Van Acker <lieven.vanacker@punchtelematix.net>
2011-12-06 16:39:37 +0100

    Fix naming of xml file
    Signed-off-by: Lieven Van Acker <lieven.vanacker@punchtelematix.net>

--- src/main/java/net/sourceforge/schemaspy/view/HtmlMainIndexPage.java
+++ src/main/java/net/sourceforge/schemaspy/view/HtmlMainIndexPage.java
@@ -155,10 +155,11 @@ public class HtmlMainIndexPage extends HtmlFormatter {
         html.writeln(" <tr>");
         html.write("  <td class='container'>");
         String xmlName = db.getName();
-        if (db.getSchema() != null)
-            xmlName += '.' + db.getSchema();
-        else if (db.getCatalog() != null)
-            xmlName += '.' + db.getCatalog();
+        // This generates the wrong xmlname....
+        // if (db.getSchema() != null)
+        //    xmlName += '.' + db.getSchema();
+        // else if (db.getCatalog() != null)
+        //    xmlName += '.' + db.getCatalog();
         html.write("<br><a href='" + xmlName + ".xml' title='XML Representation'>XML Representation</a>");
         html.write("<br><a href='insertionOrder.txt' title='Useful for loading data into a database'>Insertion Order</a>&nbsp;");
         html.write("<a href='deletionOrder.txt' title='Useful for purging data from a database'>Deletion Order</a>");
